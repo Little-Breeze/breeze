a9087fb9badd92758d52808fc778ce146bc25a26
zhangn <ning.zhang@dianrong.com>
2018-11-09 17:21:09 +0800

    update redux template

--- templates/redux/breeze.config.js
+++ templates/redux/breeze.config.js
@@ -1,49 +1,55 @@
 module.exports = {
     // debug host
-    "host": "0.0.0.0",
+    host: "0.0.0.0",
   
     // debug port
-    "port": "9527",
+    port: "9527",
   
-    // pepper src entry, also inner webpack entry, default to `src/pages/index.js`
-    "base": "src",
+    // breeze default src dir
+    base: "src",
+  
+    prerender: false, // 是否开启预渲染
+  
+    // webpack entry, default to `src/index.js` 单页面入口
+    entry: "index.js",
+  
+    // 定义多页面入口
+    pages: {
+      name: "pages", // custom default `page` dir
+      multi: ''
+    },
   
     // target build dir
-    "build": "dist",
+    build: "dist",
   
-    // CDN domain, or just leave it blank
-    "static": {
-        "start"         :   "",                    // here use relative path
-        "test"          :   "",
-        "pre"           :   "//static.test.com/",// here use CDN domain
-        "release"       :   "//static.test.com/" // here use CDN domain
+    // static domain
+    static: {
+      start: "", // relative path
+      test: "",
+      pre: "//static.test.com/", // CDN domain
+      release: "//static.test.com/" // CDN domain
     },
   
-    "api": {  
-        "start"       :   "",
-        "test"        :   "",
-        "pre"         :   "//api.test.com",
-        "release"     :   "//api.test.com"
+    api: {
+      start: "",
+      test: "",
+      pre: "//api.test.com",
+      release: "//api.test.com"
     },
+  
     // globals
-    "globals": {},  // 配置全局变量
+    globals: {}, // 配置全局变量
   
     // third patry libs to bundle
-    "vendor": ["react", "react-dom"],
+    vendor: ["react", "react-dom"],
   
     // switch for eslint
-    "eslint": false,
-  
-    // custom default page dir
-    "pages": "pages",
+    eslint: false,
   
     // custom default component dir
-    "components": "components",
-    
-    // switch for transfering assets dir to dist when build
-    // "transfer_assets": false,
+    components: "components",
   
     // limit image size for use base64, (smaller use base64, larger use url)
-    "base64_image_limit": 10240 // 10k
-  }
+    base64_image_limit: 10240 // 10k
+  };
   
